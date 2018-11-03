189652b417105635a9809ecd6b9d0f7379690f8d
SanderHestvik <sander_hestvik@hotmail.com>
2018-11-03 17:29:29 +0100

    Fixed a few code errors

--- HTTPSserverMQTT.py
+++ HTTPSserverMQTT.py
@@ -1,27 +1,28 @@
 from flask import Flask, request
 import paho.mqtt.client as mqtt
 app = Flask(__name__)
-topic = "teknobyen/doors/front/open2"
-
+topic = "teknobyen/doors/front/open"
+client = mqtt.Client()
 
 def on_connect(client, userdate, flags, rc):
     client.publish(topic, payload=1)
+    client.loop_stop(force=False)
 
 
 @app.route("/")
 def open_door():
     username = request.args.get("username")
-    print(username)
     password = request.args.get("password")
-    print(password)
     print("creating new instance")
-    client = mqtt.Client()
+
     client.username_pw_set(username, password)
     client.tls_set()
     print("connecting")
     client.on_connect = on_connect
     client.connect("mqttbroker.tk", 8883)
-    client.loop_forever()
+    client.loop_start()
+
+    return "Opening door!"
 
 
 
